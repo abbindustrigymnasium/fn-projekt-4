a81520426d7a5bfcf265517668612a5aa9c1cdf4
abbrobvik <robin.vikstedt@abbgymnasiet.se>
2023-02-07 12:20:07 +0100

    user

---
+++ user.ts
@@ -0,0 +1,35 @@
+import express from "express"
+import prisma from "./prisma"
+import {app} from "./app"
+
+app.post("/users", async (req, res) => {
+    try {
+      const { email, name, password } = req.body
+      // games is an array of string | string[]
+      const newUser = await prisma.user.create({
+        data: {
+            email,
+            name,
+            password
+        },
+      })
+      res.json(newUser)
+    } catch (error: any) {
+      console.log(error.message)
+      res.status(500).json({
+        message: "Internal Server Error",
+      })    
+    }
+  })
+
+app.get("/users", async (req, res) => {
+    try {
+        const users = await prisma.user.findMany()
+
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+        })
+    }
+})
