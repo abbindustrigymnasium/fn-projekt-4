ffa77876be095e37c6152599131b311b77ad56f8
abbviksko <viktor.skold@abbgymnasiet.se>
2023-02-13 14:09:32 +0100

    CORS policy fixes and folder migrations

--- app.ts
+++ app.ts
@@ -1,11 +1,173 @@
-import express from "express";
-import prisma from "./prisma"; // importing the prisma instance we created.
+import express from "express"
+import prisma from "./prisma" // importing the prisma instance we created.
 
-const app = express();
-app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const app = express()
+app.use(express.json())
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const PORT = process.env.PORT || 3030
 
-export { app };
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+
+app.post("/users/create", async (req, res) => {
+    try {
+      const { email, name, password } = req.body
+      // games is an array of string | string[]
+      const user = await prisma.user.create({
+       data: {
+            email,
+            name,
+            password
+        }, 
+      })
+      console.log(user)
+      res.json(user)
+    } catch (error: any) {
+      console.log(error.message)
+      res.status(500).json({
+        message: "Internal Server Error",
+      })    
+    }
+  })
+
+  app.get("/users/get", async (req, res) => {
+    try {
+        const user = await prisma.user.findMany()
+
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+        })
+    }
+})
+
+app.post("/votes/create", async (req, res) => {
+    try {
+      const { water_vote, userId } = req.body;
+      console.log(req.body)
+      const newVote = await prisma.vote.create({
+        data: {
+          userId: userId,
+          water_vote: water_vote
+        },
+      });
+      res.json(newVote);
+    } catch (error: any) {
+      console.log(error.message);
+      res.status(500).json({
+        message: "Internal Server Error",
+      });
+    }
+  });
+
+  app.get("/votes/user/:id", async (req, res) => {
+    try {
+      const id = req.params.id
+  
+      const votes = await prisma.vote.findMany({
+        where: {
+          userId: +id
+        }
+      });
+  
+      res.json(votes);
+    } catch (error) {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  });
+  
+  app.post("/countries/create", async (req, res) => {
+    try {
+      const { countryName, waterQuality, waterComsumption } = req.body
+      
+      let userScore = 0;
+  
+      const newCountry = await prisma.country.create({
+        data: {
+          countryName,
+          waterQuality,
+          waterComsumption,
+          userScore
+        },
+      })
+  
+      res.json(newCountry)
+    } catch (error: any) {
+      console.log(error.message)
+      res.status(500).json({
+        message: "Internal Server Error",
+      })
+    }
+})
+
+app.get("/countries/get", async (req, res) => {
+    try {
+      const countries = await prisma.country.findMany()
+  
+      res.json(countries)
+    } catch (error) {
+      res.status(500).json({
+        message: "Something went wrong",
+      })
+    }
+})
+
+app.delete("/countries/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const deletedCountry = await prisma.country.delete({
+        where: {
+          id : parseInt(id),
+        },
+      })
+  
+      res.json(deletedCountry)
+    } catch (error) {
+      res.status(500).json({
+        message: "Something went wrong",
+      })
+    }
+})
+
+app.post("/continent/create", async (req, res) => {
+    try {
+      const { continentName, waterQuality, waterConsumption, userScore } = req.body
+    
+      const newContinent = await prisma.continent.create({
+        data: {
+            continentName, 
+            waterQuality,
+            waterConsumption,
+            userScore
+        }, 
+      })
+      res.json(newContinent)
+    } catch (error: any) {
+      console.log(error.message)
+      res.status(500).json({
+        message: "Internal Server Error",
+      })    
+    }
+  })
+
+  app.get("/continent/get", async (req, res) => {
+    try {
+        const users = await prisma.continent.findMany()
+
+        res.json(users)
+        console.log(users)
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+        })
+    }
+})
+
+
+export{app}
+
--- continents.ts
+++ continents.ts
@@ -1,86 +1,38 @@
 import express from "express"
-import prisma from "./prisma" // importing the prisma instance we created.
+import prisma from "./prisma"
 import {app} from "./app"
 
-app.post("/continents", async (req, res) => {
+app.post("/", async (req, res) => {
     try {
-      const { continentName, waterQuality, waterComsumption } = req.body
-      
-      let userScore = 0;
-  
+      const { continentName, waterQuality, waterConsumption, userScore } = req.body
+    
       const newContinent = await prisma.continent.create({
         data: {
-          continentName,
-          waterQuality,
-          waterComsumption,
-          userScore
-        },
+            continentName, 
+            waterQuality,
+            waterConsumption,
+            userScore
+        }, 
       })
-  
       res.json(newContinent)
     } catch (error: any) {
       console.log(error.message)
       res.status(500).json({
         message: "Internal Server Error",
-      })
+      })    
+    }
+  })
+
+app.get("/", async (req, res) => {
+    try {
+        const users = await prisma.continent.findMany()
+
+        res.json(users)
+        console.log(users)
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong",
+        })
     }
 })
 
-app.get("/continents", async (req, res) => {
-    try {
-      const continents = await prisma.continent.findMany()
-  
-      res.json(continents)
-    } catch (error) {
-      res.status(500).json({
-        message: "Something went wrong",
-      })
-    }
-})
-
-app.delete("/continents/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const deletedContinent = await prisma.continent.delete({
-        where: {
-          id : parseInt(id),
-        },
-      })
-  
-      res.json(deletedContinent)
-    } catch (error) {
-      res.status(500).json({
-        message: "Something went wrong",
-      })
-    }
-})
-/*
-app.put("/users/:id", async (req, res) => {
-    try {
-      const { name, games } = req.body
-      const { id } = req.params
-  
-      const updatedUser = await prisma.user.update({
-        where: {
-          id,
-        },
-        data: {
-          name,
-          games: {
-            connectOrCreate: games.map((game: string) => ({
-              where: { name: game },
-              create: { name: game },
-            })),
-          },
-        },
-      })
-  
-      res.json(updatedUser)
-    } catch (error) {
-      res.status(500).json({
-        message: "Something went wrong",
-      })
-    }
-})
-*/
