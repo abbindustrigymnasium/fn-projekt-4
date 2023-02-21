7757e1afb3c4f738b75c54adecd5cd0854aa8e15
abbviksko <viktor.skold@abbgymnasiet.se>
2023-02-21 14:48:54 +0100

    yes

--- prisma.ts
+++ prisma.ts
@@ -4,20 +4,3 @@ const prisma = new PrismaClient()
 
 export default prisma
 
-async function main() {
-    const newUser = await prisma.user.create({
-      data: {
-        name: "viktor",
-        email: "viktor.skoeld@outlook.com"
-      },
-    });
-    console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
-  }
-  
-  main()
-    .catch((e) => {
-      console.error(e);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
